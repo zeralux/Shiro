955ed0d7081c3744271caca09d27dc9bbb9126f1
david9453 <Zeralux@gmail.com>
2020-02-27 23:21:48 +0800

    修改

--- src/main/java/person/project/ShiroApplication.java
+++ src/main/java/person/project/ShiroApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShiroApplication {
 
 	public static void main(String[] args) {
+		System.out.println("GOGOGOGOGOG");
 		SpringApplication.run(ShiroApplication.class, args);
 	}
 
