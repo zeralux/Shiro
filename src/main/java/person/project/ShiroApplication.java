b4fb227728054df02ba0f1fda5804be41a47636c
david9453 <Zeralux@gmail.com>
2020-02-28 18:59:51 +0800

    david 增加功能

--- src/main/java/person/project/ShiroApplication.java
+++ src/main/java/person/project/ShiroApplication.java
@@ -8,6 +8,9 @@ public class ShiroApplication {
 
 	public static void main(String[] args) {
 		System.out.println("GOGOGOGOGOG");
+		System.out.println("GOGOGOGOGOG");
+		System.out.println("GOGOGOGOGOG");
+		System.out.println("GOGOGOGOGOG");
 		SpringApplication.run(ShiroApplication.class, args);
 		System.out.println("NO");
 	}
